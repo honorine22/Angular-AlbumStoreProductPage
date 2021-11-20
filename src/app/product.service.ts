d6dae0579370c12fd9324ad7d229b09123f86fa3
honorine22 <higiranez@gmail.com>
2021-11-20 18:08:15 +0200

    Module 3 works!

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,4 +1,4 @@
-import { map } from "rxjs/operators";
+import "rxjs/operator/map";
 import { Http, Response } from "@angular/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
