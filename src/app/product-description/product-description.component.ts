f8c0d13374e89e283d2e22f990496315049bb3ff
honorine22 <higiranez@gmail.com>
2021-11-20 18:03:19 +0200

    Module 3 works!

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,18 +1,19 @@
 import { ProductService } from "../product.service";
 import { Component, OnInit } from "@angular/core";
+import { Album } from "app/album";
 @Component({
   selector: "app-product-description",
   templateUrl: "./product-description.component.html",
   styleUrls: ["./product-description.component.css"],
 })
 export class ProductDescriptionComponent implements OnInit {
-  albumInfo;
+  albumInfo: Album;
 
   constructor(private _productService: ProductService) {}
 
   ngOnInit() {
-    this._productService.getAlbum(1).subscribe((response) => {
-      this.albumInfo = response;
-    });
+    this._productService
+      .getAlbum(1)
+      .subscribe((response) => (this.albumInfo = response));
   }
 }
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,12 +1,13 @@
+import { map } from "rxjs/operators";
 import { Http, Response } from "@angular/http";
 import { Injectable } from "@angular/core";
-import "rxjs/operator/map";
+import { Observable } from "rxjs";
+import { Album } from "./album";
 @Injectable()
 export class ProductService {
   private _albumUrl: string = "../assets/album.json";
   constructor(private _http: Http) {}
-
-  getAlbum(id: number) {
-    return this._http.get(this._albumUrl).map((response) => response.json());
+  getAlbum(id: number): Observable<Album> {
+    return this._http.get(this._albumUrl).map(response => <Album>response.json());
   }
 }
